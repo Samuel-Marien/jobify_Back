f34789243d6845987b30685b45de350d6935e2ed
Samuel <timeuss.sam@gmail.com>
2022-09-26 16:53:25 +0200

    register user initial setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,16 @@
-const register = async (req, res) => {
-  res.send('register user')
+import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
+
+const register = async (req, res, next) => {
+  // Ce try catch ou alors le package "express async error"
+  // try {
+  //   const user = await User.create(req.body)
+  //   res.status(201).json({ user })
+  // } catch (error) {
+  //   next(error)
+  // }
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({ user })
 }
 const login = async (req, res) => {
   res.send('login user')
