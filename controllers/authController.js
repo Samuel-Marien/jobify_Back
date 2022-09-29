64164dea4b4ce9150d665da16fa000442a0bc15e
Samuel <timeuss.sam@gmail.com>
2022-09-29 22:31:15 +0200

    add jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,24 @@ const register = async (req, res, next) => {
   }
 
   const user = await User.create({ name, email, password })
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = user.createJWT()
+
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name
+    },
+    token,
+    location: user.location
+  })
 }
+
 const login = async (req, res) => {
   res.send('login user')
 }
+
 const updateUser = async (req, res) => {
   res.send('updateUser user')
 }
